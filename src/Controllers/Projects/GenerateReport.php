fbc638dbeb8bcf675a9bb072a37e3760e9839704
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-08-19 18:47:47 +0100

    Add generate PDF report option

--- src/Controllers/Projects/GenerateReport.php
+++ src/Controllers/Projects/GenerateReport.php
@@ -10,6 +10,8 @@ use Reconmap\Controllers\Controller;
 use Reconmap\Repositories\ProjectRepository;
 use Reconmap\Repositories\TaskRepository;
 use Reconmap\Repositories\VulnerabilityRepository;
+use Dompdf\Dompdf;
+use Laminas\Diactoros\CallbackStream;
 
 class GenerateReport extends Controller
 {
@@ -17,6 +19,8 @@ class GenerateReport extends Controller
 	public function __invoke(ServerRequestInterface $request, array $args): ResponseInterface
 	{
 		$id = (int)$args['id'];
+		$params = $request->getQueryParams();
+		$format = $params['format'] ?? 'html';
 
 		$repository = new ProjectRepository($this->db);
 		$project = $repository->findById($id);
@@ -43,8 +47,8 @@ class GenerateReport extends Controller
 		$vulnerabilityRepository = new VulnerabilityRepository($this->db);
 		$vulnerabilities = $vulnerabilityRepository->findByProjectId($id);
 
-		foreach($vulnerabilities as $vuln) {
-			$html.= <<<HTML
+		foreach ($vulnerabilities as $vuln) {
+			$html .= <<<HTML
 			<li><strong>{$vuln['summary']}</strong></li>
 			HTML;
 		}
@@ -52,8 +56,28 @@ class GenerateReport extends Controller
 		$html .= '</ul>';
 
 		$response = new \GuzzleHttp\Psr7\Response;
-		$response->getBody()->write($html);
-		return $response->withHeader('Access-Control-Allow-Origin', '*')
-			->withHeader('Content-type', 'text/html');
+
+		if ($format === 'html') {
+			$response = new \GuzzleHttp\Psr7\Response;
+			$response->getBody()->write($html);
+			return $response->withHeader('Access-Control-Allow-Origin', '*')
+				->withHeader('Content-type', 'text/html');
+		} else {
+			$dompdf = new Dompdf();
+			$dompdf->loadHtml($html);
+
+			$dompdf->setPaper('A4', 'landscape');
+			$dompdf->render();
+			$body = new CallbackStream(function () use ($dompdf) {
+				$dompdf->stream();
+			});
+			$filename = 'report.pdf';
+			return $response
+				->withBody($body)
+				->withHeader('Access-Control-Expose-Headers', 'Content-Disposition')
+				->withHeader('Access-Control-Allow-Origin', '*')
+				->withHeader('Content-Disposition', 'attachment; filename="' . $fileName . '";')
+				->withHeader('Content-type', 'application/pdf');
+		}
 	}
 }
