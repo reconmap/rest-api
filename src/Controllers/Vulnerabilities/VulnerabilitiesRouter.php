79dae716e86c877b6ee9598cca57ce2a8affd4d6
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-09-06 20:29:09 +0100

    Add create vulnerability endpoint

--- src/Controllers/Vulnerabilities/VulnerabilitiesRouter.php
+++ src/Controllers/Vulnerabilities/VulnerabilitiesRouter.php
@@ -17,6 +17,7 @@ class VulnerabilitiesRouter
     public function mapRoutes(RouteCollectionInterface $router): void
     {
         $router->map('GET', '/vulnerabilities', GetVulnerabilitiesController::class);
+        $router->map('POST', '/vulnerabilities', CreateVulnerabilityController::class);
         $router->map('GET', '/vulnerabilities/stats', GetVulnerabilitiesStatsController::class);
         $router->map('GET', '/vulnerabilities/{id:number}', GetVulnerabilityController::class);
         $router->map('PATCH', '/vulnerabilities/{id:number}', UpdateVulnerabilityController::class);
