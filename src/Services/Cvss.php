76b8b3e8f6ed9eb651571e138688e37ab49bf7d9
Braunson Yager <Braunson@users.noreply.github.com>
2020-10-03 12:59:42 -0400

    Added missing break's

--- src/Services/Cvss.php
+++ src/Services/Cvss.php
@@ -16,14 +16,19 @@ class Cvss
         switch (true) {
             case $score == 0:
                 $severity = 'none';
+                break;
             case self::betweenValue($score, 0.1, 3.9):
                 $severity = 'low';
+                break;
             case self::betweenValue($score, 4.0, 6.9):
                 $severity = 'medium';
+                break;
             case self::betweenValue($score, 7.0, 8.9):
                 $severity = 'high';
+                break;
             case $score >= 9.0:
                 $severity = 'critical';
+                break;
             default:
                 throw new \InvalidArgumentException("Invalid CVSS score: $score");
         }
