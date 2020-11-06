14ada03a98e7de640b6efa276fb1e7d390d66320
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-11-06 17:18:44 +0000

    Reduce complexity in Cvss::toSeverity code

--- src/Services/Cvss.php
+++ src/Services/Cvss.php
@@ -13,26 +13,20 @@ class Cvss
 
     static public function toSeverity(float $score): string
     {
-        switch (true) {
-            case $score == 0:
-                $severity = 'none';
-                break;
-            case self::betweenValue($score, 0.1, 3.9):
-                $severity = 'low';
-                break;
-            case self::betweenValue($score, 4.0, 6.9):
-                $severity = 'medium';
-                break;
-            case self::betweenValue($score, 7.0, 8.9):
-                $severity = 'high';
-                break;
-            case $score >= 9.0:
-                $severity = 'critical';
-                break;
-            default:
-                throw new \InvalidArgumentException("Invalid CVSS score: $score");
+        if ($score == 0) {
+            $severity = 'none';
+        } elseif (self::betweenValue($score, 0.1, 3.9)) {
+            $severity = 'low';
+        } elseif (self::betweenValue($score, 4.0, 6.9)) {
+            $severity = 'medium';
+        } elseif (self::betweenValue($score, 7.0, 8.9)) {
+            $severity = 'high';
+        } elseif ($score >= 9.0) {
+            $severity = 'critical';
+        } else {
+            throw new \InvalidArgumentException("Invalid CVSS score: $score");
         }
-        
+
         return $severity;
     }
 }
