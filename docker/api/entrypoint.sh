968398b3df57c271c6d7c62d4b802112f102b793
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-02-16 20:24:56 +0000

    Upgrade to PHP8

--- docker/api/entrypoint.sh
+++ docker/api/entrypoint.sh
@@ -2,8 +2,8 @@
 
 service cron start
 
-# service php7.4-fpm start does not pass env variables to process.
-/etc/init.d/php7.4-fpm start
+# 'service php8.0-fpm start' does not pass env variables to process.
+/etc/init.d/php8.0-fpm start
 
 # Hand off to the CMD
 exec "$@"
