c13777ac59f5b9a4ebe25ea3c3ad851a8797788a
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-04-06 19:34:18 +0100

    Fix issue with multiple origins

--- src/CorsMiddleware.php
+++ src/CorsMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace Reconmap;
 
+use Monolog\Logger;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -10,24 +11,30 @@ use Reconmap\Services\ApplicationConfig;
 
 class CorsMiddleware implements MiddlewareInterface
 {
-    public function __construct(private ApplicationConfig $config)
+    public function __construct(private Logger $logger,
+                                private ApplicationConfig $config)
     {
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $response = $handler->handle($request);
-        return $this->decorateResponse($response);
-    }
 
-    private function decorateResponse(ResponseInterface $response): ResponseInterface
-    {
-        $corsConfig = $this->config->getSettings('cors');
-        $allowedOrigins = implode(',', $corsConfig['allowedOrigins']);
-
-        return $response
+        $response = $response
             ->withHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH')
-            ->withHeader('Access-Control-Allow-Headers', 'Authorization,Bulk-Operation,Content-Type')
-            ->withHeader('Access-Control-Allow-Origin', $allowedOrigins);
+            ->withHeader('Access-Control-Allow-Headers', 'Authorization,Bulk-Operation,Content-Type');
+
+        if ($request->hasHeader('Origin')) {
+            $requestOrigin = $request->getHeaderLine('Origin');
+            $corsConfig = $this->config->getSettings('cors');
+
+            if (in_array($requestOrigin, $corsConfig['allowedOrigins'])) {
+                $response = $response->withHeader('Access-Control-Allow-Origin', $requestOrigin);
+            } else {
+                $this->logger->warning("Invalid origin: $requestOrigin");
+            }
+        }
+
+        return $response;
     }
 }
