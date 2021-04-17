3554b4e740d521168d7544a2a2939c78e1dc5090
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-04-17 21:17:46 +0100

    Add AttachemtnFilePath test

---
+++ tests/Services/AttachmentFilePathTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Reconmap\Services;
+
+use PHPUnit\Framework\TestCase;
+
+class AttachmentFilePathTest extends TestCase
+{
+    private $mockApplicationConfig;
+    private AttachmentFilePath $subject;
+
+    public function setUp(): void
+    {
+        $this->mockApplicationConfig = $this->createMock(ApplicationConfig::class);
+        $this->mockApplicationConfig->expects($this->once())
+            ->method('getAppDir')
+            ->willReturn('/app/reconmap');
+
+        $this->subject = new AttachmentFilePath($this->mockApplicationConfig);
+    }
+
+    public function testGenerateBasePath()
+    {
+        $this->assertEquals('/app/reconmap/data/attachments/', $this->subject->generateBasePath());
+    }
+
+    public function testGenerateFilePath()
+    {
+        $this->assertEquals('/app/reconmap/data/attachments/test.file', $this->subject->generateFilePath('test.file'));
+    }
+
+    public function testGenerateFilePathFromAttachment()
+    {
+        $this->assertEquals('/app/reconmap/data/attachments/document.txt', $this->subject->generateFilePathFromAttachment(['file_name' => 'document.txt']));
+    }
+}
