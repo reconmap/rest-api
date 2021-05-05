ba7a51cb53ba25b27c7ed5a594a0a432b056526f
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-05-05 21:28:46 +0100

    Add basic test coverage to controller

---
+++ tests/Controllers/Tasks/UpdateTaskControllerTest.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Reconmap\Controllers\Tasks;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use Reconmap\Models\AuditLogAction;
+use Reconmap\Repositories\TaskRepository;
+use Reconmap\Services\ActivityPublisherService;
+
+class UpdateTaskControllerTest extends TestCase
+{
+    public function testHappyPath()
+    {
+        $fakeTaskId = 49;
+
+        $mockRequest = $this->createMock(ServerRequestInterface::class);
+        $mockRequest->expects($this->once())
+            ->method('getBody')
+            ->willReturn('{"summary": "new task name"}');
+        $mockRequest->expects($this->once())
+            ->method('getAttribute')
+            ->with('userId')
+            ->willReturn(9);
+
+        $mockTaskRepository = $this->createMock(TaskRepository::class);
+        $mockTaskRepository->expects($this->once())
+            ->method('updateById')
+            ->with($fakeTaskId, ['summary' => 'new task name'])
+            ->willReturn(true);
+
+        $mockPublisherService = $this->createMock(ActivityPublisherService::class);
+        $mockPublisherService->expects($this->once())
+            ->method('publish')
+            ->with(9, AuditLogAction::TASK_MODIFIED, ['type' => 'task', 'id' => $fakeTaskId]);
+
+        $args = ['taskId' => $fakeTaskId];
+
+        $controller = new UpdateTaskController($mockTaskRepository, $mockPublisherService);
+        $response = $controller($mockRequest, $args);
+        $this->assertEquals(['success' => true], $response);
+    }
+}
