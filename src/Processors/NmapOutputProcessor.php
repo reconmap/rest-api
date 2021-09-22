0007e4c600430e3ced6f0c71c313a8894cc44eed
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-09-22 23:25:28 +0100

    Parse target from nmap results

--- src/Processors/NmapOutputProcessor.php
+++ src/Processors/NmapOutputProcessor.php
@@ -12,13 +12,18 @@ class NmapOutputProcessor extends AbstractCommandParser implements Vulnerability
         $vulnerabilities = [];
 
         $xml = simplexml_load_file($path);
-        foreach ($xml->host->ports->port as $port) {
-            if ((string)$port->state['state'] == 'open') {
-                $portNumber = (int)$port['portid'];
-                $vulnerability = new Vulnerability;
-                $vulnerability->summary = "Port $portNumber has been left open.";
-                $vulnerability->description = "The port $portNumber is open and could be used by an attacker to get into your system. Unless you need this port open consider shutting the service down or restricting access using a firewall.";
-                $vulnerabilities[] = $vulnerability;
+        foreach ($xml->host as $host) {
+            $hostAddress = $host->address['addr'];
+            foreach ($host->ports->port as $port) {
+                if ((string)$port->state['state'] == 'open') {
+                    $portNumber = (int)$port['portid'];
+                    $vulnerability = new Vulnerability;
+                    $vulnerability->summary = "Port $portNumber has been left open.";
+                    $vulnerability->description = "The port $portNumber is open and could be used by an attacker to get into your system. Unless you need this port open consider shutting the service down or restricting access using a firewall.";
+
+                    $vulnerability->host = (object)['name' => (string)$hostAddress];
+                    $vulnerabilities[] = $vulnerability;
+                }
             }
         }
 
