8c6904150843f45e975c1634d7f7449969c11bb7
ganoes <gano3s@protonmail.com>
2022-03-01 17:01:07 +0100

    Added CreateVaultItemControllerTest

---
+++ tests/Controllers/Vault/CreateVaultItemControllerTest.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace Reconmap\Controllers\Vault;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use Reconmap\Models\Vault;
+use Reconmap\Repositories\VaultRepository;
+use Reconmap\Models\AuditActions\VaultAuditActions;
+use Reconmap\Services\AuditLogService;
+use Fig\Http\Message\StatusCodeInterface;
+
+class CreateVaultItemControllerTest extends TestCase
+{
+    public function testHappyPath()
+    {
+        $project_id = 3;
+
+        $mockRequest = $this->createMock(ServerRequestInterface::class);
+        $mockRequest->expects($this->once())
+            ->method('getBody')
+            ->willReturn('{"name":"compromised account","value":"secret text","note":"Some note for other testers","reportable":false,"type":"password","password":"UltimateP4ssw0rd!"}');
+        $mockRequest->expects($this->once())
+            ->method('getAttribute')
+            ->with('userId')
+            ->willReturn(7);
+
+        $mockAuditLogService = $this->createMock(AuditLogService::class);
+        $mockAuditLogService->expects($this->once())
+            ->method('insert')
+            ->with(7, VaultAuditActions::ITEM_CREATED, ['compromised account']);
+
+        $vault = new Vault();
+        $vault->name = "compromised account";
+        $vault->value = "secret text";
+        $vault->note = "Some note for other testers";
+        $vault->reportable = false;
+        $vault->type = "password";
+        $vault->project_id = $project_id;
+
+        $mockRepository = $this->createMock(VaultRepository::class);
+        $mockRepository->expects($this->once())
+        ->method('insert')
+        ->with($vault, "UltimateP4ssw0rd!")
+        ->willReturn(2);
+
+        $args = ['projectId' => $project_id];
+
+        $controller = new CreateVaultItemController($mockRepository, $mockAuditLogService);
+        $response = $controller($mockRequest, $args);
+        $this->assertEquals(StatusCodeInterface::STATUS_CREATED, $response->getStatusCode());
+    }
+}
