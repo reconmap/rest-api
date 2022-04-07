4cf5e8625cca07c137e48ac68329bebbeb36d635
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2022-04-07 19:45:03 +0100

    Add foreign key change to SQL migration file

--- database/migrations/migration-10200-10300.sql
+++ database/migrations/migration-10200-10300.sql
@@ -3,3 +3,7 @@ ALTER TABLE command
 
 ALTER TABLE audit_log
     MODIFY COLUMN user_id INT UNSIGNED NULL COMMENT 'Null is system';
+
+ALTER TABLE vulnerability
+    DROP FOREIGN KEY vulnerability_ibfk_2,
+    ADD CONSTRAINT vulnerability_fk_project_id FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE;
