5660533e8ad5831715eff1bd5b7d92eb14199353
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2024-11-07 00:00:29 +0000

    Return more info from usage and health controllers

--- src/Controllers/System/GetSystemUsageController.php
+++ src/Controllers/System/GetSystemUsageController.php
@@ -5,15 +5,23 @@ namespace Reconmap\Controllers\System;
 use Psr\Http\Message\ServerRequestInterface;
 use Reconmap\Controllers\Controller;
 use Reconmap\Repositories\AttachmentRepository;
+use Reconmap\Services\RedisServer;
 
 class GetSystemUsageController extends Controller
 {
-    public function __construct(private readonly AttachmentRepository $attachmentRepository)
+    public function __construct(private readonly AttachmentRepository $attachmentRepository, private readonly RedisServer $redisServer)
     {
     }
 
     public function __invoke(ServerRequestInterface $request): array
     {
-        return ['attachments' => $this->attachmentRepository->getUsage()];
+        return [
+            'attachments' => $this->attachmentRepository->getUsage(),
+            'queueLengths' => [
+                'emails' => $this->redisServer->lLen('email:queue'),
+                'tasks' => $this->redisServer->lLen('tasks:queue'),
+                'notifications' => $this->redisServer->lLen('notifications:queue'),
+            ]
+        ];
     }
 }
--- src/Services/RedisServer.php
+++ src/Services/RedisServer.php
@@ -4,20 +4,26 @@ namespace Reconmap\Services;
 
 class RedisServer extends \Redis
 {
+    /**
+     * @throws \RedisException
+     * @throws \Exception
+     */
     public function __construct(Environment $env)
     {
+        parent::__construct();
+
         $host = $env->getValue('REDIS_HOST');
         $port = $env->getValue('REDIS_PORT');
 
         if (false === $this->connect($host, (int)$port)) {
-            throw new Exception("Unable to connect to Redis server $host:$port");
+            throw new \Exception("Unable to connect to Redis server $host:$port");
         }
 
         $user = $env->getValue('REDIS_USER');
         $password = $env->getValue('REDIS_PASSWORD');
 
         if (false === $this->auth([$user, $password])) {
-            throw new Exception('Unable to authenticate to Redis');
+            throw new \Exception('Unable to authenticate to Redis');
         }
     }
 }
