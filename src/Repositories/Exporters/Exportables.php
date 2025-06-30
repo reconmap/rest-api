fbbff04ae861eed6f6a10f5b98ee51f68a428783
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2025-06-30 19:32:20 +0100

    Rename clients to orgs in exportable list

--- src/Repositories/Exporters/Exportables.php
+++ src/Repositories/Exporters/Exportables.php
@@ -6,13 +6,13 @@ class Exportables
 {
     public const array List = [
         ['key' => 'audit_log', 'description' => 'Audit log', 'className' => AuditLogExporter::class],
-        ['key' => 'clients', 'description' => 'Clients', 'className' => ClientsExporter::class],
         ['key' => 'commands', 'description' => 'Commands', 'className' => CommandsExporter::class],
         ['key' => 'documents', 'description' => 'Documents', 'className' => DocumentsExporter::class],
         ['key' => 'projects', 'description' => 'Projects', 'className' => ProjectsExporter::class],
         ['key' => 'project_templates', 'description' => 'Project templates', 'className' => ProjectTemplatesExporter::class],
         ['key' => 'tasks', 'description' => 'Tasks', 'className' => TasksExporter::class],
         ['key' => 'targets', 'description' => 'Targets', 'className' => TargetsExporter::class],
+        ['key' => 'organisations', 'description' => 'Organisations', 'className' => ClientsExporter::class],
         ['key' => 'users', 'description' => 'Users', 'className' => UsersExporter::class],
         ['key' => 'vulnerabilities', 'description' => 'Vulnerabilities', 'className' => VulnerabilitiesExporter::class],
         ['key' => 'vulnerability_categories', 'description' => 'Vulnerability categories', 'className' => VulnerabilityCategoriesExporter::class],
