d0aa973253215d6a0bbca770c8a8bacb3d25dd79
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2026-02-23 22:30:00 +0000

    Remove all PHP code

--- integrations-api/src/Repositories/Findable.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Reconmap\Repositories;
-
-interface Findable
-{
-    public function findById(int $id): array|object|null;
-}
--- integrations-api/src/Repositories/Updateable.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Reconmap\Repositories;
-
-interface Updateable
-{
-    public function updateById(int $id, array $newColumnValues): bool;
-}
