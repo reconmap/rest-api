0a02f63256aaa365e651598598d8bf585f757d64
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2026-03-15 17:56:27 +0000

    Add unit test for PaginationRequestHandler

--- app/Common/PaginationRequestHandler.cs
+++ app/Common/PaginationRequestHandler.cs
@@ -4,7 +4,7 @@ public class PaginationRequestHandler(IQueryCollection query, int totalCount)
 {
     public int GetResultsPerPage()
     {
-        return query.TryGetValue("limit", out var limit) ? int.Parse(limit) : 20;
+        return query.TryGetValue("limit", out var limit) && int.TryParse(limit, out var result) ? result : 20;
     }
 
     public int CalculateOffset()
@@ -14,6 +14,6 @@ public class PaginationRequestHandler(IQueryCollection query, int totalCount)
 
     public int CalculatePageCount()
     {
-        return Math.Max(1, (int)Math.Ceiling((double)(totalCount / GetResultsPerPage())));
+        return Math.Max(1, (int)Math.Ceiling((double)totalCount / GetResultsPerPage()));
     }
 }
